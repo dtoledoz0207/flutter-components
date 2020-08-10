0daeb96d86525b31ead1bd062121d0a7b580a5cb
David Toledo <dtoledoz0207@gmail.com>
2020-08-10 10:42:44 -0500

    create-createItems-Method-to-create-all-the-ListTile-for-the-ListView-with-the-options-variable

--- lib/pages/home_temp.dart
+++ lib/pages/home_temp.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+
+  final List<String> options = ['One','Two','Three','Four','Five'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,20 +11,24 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Temp Components'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('ListTile title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile title'),
-          )
-        ],
+        children: _createItems()
       ),
     );
+  }
+
+  List<Widget> _createItems() {
+
+    List<Widget> list = new List<Widget>();
+
+    for (String option in options) {
+      final tempItemWidget = ListTile(
+        title: Text(option),
+      );
+
+      list..add(tempItemWidget)
+          ..add(Divider(thickness: 2.0, color: Colors.deepPurple,));
+    }
+
+    return list;
   }
 }
