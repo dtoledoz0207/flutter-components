411625c3c3ae69ae0ef75a2ce1e472028ed47e73
David Toledo <dtoledoz0207@gmail.com>
2020-08-15 16:08:22 -0500

    add-circleAvatar-Widget-in-avatarPage

--- lib/pages/avatar_page.dart
+++ lib/pages/avatar_page.dart
@@ -5,8 +5,32 @@ class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Avatar Page'),),
-
+      appBar: AppBar(
+        title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1018943227791982592/URnaMrya_400x400.jpg'),
+              radius: 23.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.orangeAccent,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://i1.wp.com/www.sopitas.com/wp-content/uploads/2018/12/stan-lee-ilustracion.jpg'),
+          fadeInDuration: Duration(milliseconds: 300),
+        ),
+      ),
     );
   }
 }
